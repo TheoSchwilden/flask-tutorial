30176103715530202c6a1256bebfde47a5fde49e
theoSchwilden <theoschwilden@gmail.com>
2024-08-24 08:58:14 +0200

    initialized db

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,8 +8,10 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),    
     )
+    
+    
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -29,4 +31,8 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
+    
+    from . import db
+    db.init_app(app)
+    
     return app
